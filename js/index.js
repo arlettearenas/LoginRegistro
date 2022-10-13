041398f2c78a780cc1a972e9f976c6fc8b7d3528
arlette arenas <arlettearenas29@gmail.com>
2022-10-13 13:26:16 -0500

    actualizando datos

---
+++ js/index.js
@@ -0,0 +1,24 @@
+//declarar variables para vincular los objetos del formulario
+const usuario = document.getElementById('user');
+const password= document.getElementById('pass');
+const formulario = document.getElementById('formLogin');
+
+//Leyendo eventos
+formulario.addEventListener('submit', login);
+
+//Funciones a ejecutar
+function login(e){
+    e.preventDefault();
+    
+    let usuarioVal = usuario.value;
+    let passwordVal = password.value;
+
+if(usuarioVal == '' || passwordVal == '');{
+    return;
+}
+
+    console.log('El valor para user es: ' + usuarioVal);
+    console.log('El valor para pass es: ' + passwordVal);
+
+
+}
